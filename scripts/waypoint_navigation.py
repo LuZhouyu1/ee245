9d09624a9e4ca32e8fcafc6ce4c42ac0f7664a00
Zhouyu Lu <zlu044@ucr.edu>
2019-06-05 15:44:28 -0700

    add trajectory 8 and circle

--- scripts/waypoint_navigation.py
+++ scripts/waypoint_navigation.py
@@ -13,8 +13,12 @@ class Design_trajectory:
         self.cf=cf
         if shape == "square":
             self.trajectory_square_wp()
+        elif shape == "circle":
+            self.trajectory_circle_wp()
+        elif shape == "eight":
+            self.trajectory_eight_wp()
         else:
-            print "future work"
+            print "not a defined shape"
 
     def trajectory_square_wp(self):
         # record waypoints
@@ -23,6 +27,53 @@ class Design_trajectory:
         self.waypoints=np.array([[ini_Pos.x+s_len,ini_Pos.y,ini_Pos.z],[ini_Pos.x+s_len,ini_Pos.y+s_len,ini_Pos.z],[ini_Pos.x,ini_Pos.y+s_len,ini_Pos.z],[ini_Pos.x,ini_Pos.y,ini_Pos.z]])
         self.duration = self.total_time/4
 
+    def trajectory_circle_wp(self):
+        # record waypoints
+        self.duration=0.1
+        ini_Pos = self.cf.planner.lastKnownPosition
+        R = self.parameter
+        center = np.array([R,0,ini_Pos.z])
+        N_sample = int(self.total_time/self.duration)
+        theta = np.linspace(-math.pi,math.pi,N_sample)
+        self.waypoints=np.zeros((N_sample,3))
+        for i in range (1,N_sample):
+            self.waypoints[i-1,0] = center[0]+R*math.cos(theta[i])
+            self.waypoints[i-1,1] = center[1]+R*math.sin(theta[i])
+            self.waypoints[i-1,2] = ini_Pos.z
+        self.waypoints[N_sample-1,2] = ini_Pos.z
+
+    def trajectory_eight_wp(self):
+        # record waypoints
+        self.duration=0.1
+        ini_Pos = self.cf.planner.lastKnownPosition
+        R = self.parameter
+        center_1 = np.array([R,0,ini_Pos.z])
+        center_2 = np.array([R,2*R,ini_Pos.z])
+        N_sample_1 = int((self.total_time/self.duration*3)/8)
+        N_sample_2 = int((self.total_time/self.duration)/2)
+        N_sample_3 = int((self.total_time/self.duration)/8)
+        N_sample = N_sample_1+N_sample_2+N_sample_3
+        print N_sample_1
+        print N_sample_2
+        print N_sample_3
+        theta_1 = np.linspace(-math.pi,math.pi*1/2,N_sample_1)
+        theta_2 = np.linspace(-math.pi*1/2,math.pi*3/2,N_sample_2)
+        theta_2 = theta_2[::-1]
+        theta_3 = np.linspace(math.pi*1/2,math.pi,N_sample_3)
+        self.waypoints=np.zeros((N_sample-2,3))
+        for i in range (1,N_sample_1):
+            self.waypoints[i-1,0] = center_1[0]+R*math.cos(theta_1[i])
+            self.waypoints[i-1,1] = center_1[1]+R*math.sin(theta_1[i])
+            self.waypoints[i-1,2] = ini_Pos.z
+        for i in range (1,N_sample_2):
+            self.waypoints[i+N_sample_1-2,0] = center_2[0]+R*math.cos(theta_2[i])
+            self.waypoints[i+N_sample_1-2,1] = center_2[1]+R*math.sin(theta_2[i])
+            self.waypoints[i+N_sample_1-2,2] = ini_Pos.z
+        for i in range (1,N_sample_3):
+            self.waypoints[i+N_sample_1+N_sample_2-3,0] = center_1[0]+R*math.cos(theta_3[i])
+            self.waypoints[i+N_sample_1+N_sample_2-3,1] = center_1[1]+R*math.sin(theta_3[i])
+            self.waypoints[i+N_sample_1+N_sample_2-3,2] = ini_Pos.z
+        self.waypoints[N_sample-3,2] = ini_Pos.z
 
 if __name__ == '__main__':
 
@@ -44,10 +95,22 @@ if __name__ == '__main__':
     # Please try both goTo and cmdPosition
 
     # initialization
-    cf_dtraj=Design_trajectory("square",20.0,2.0,cf)
+    cf_dtraj=Design_trajectory("square",20.0,1.0,cf)
     n_wp=len(cf_dtraj.waypoints)
     for i in range(0,n_wp):
         cf.goTo(goal=cf_dtraj.waypoints[i],yaw=cf_dtraj.yaw,duration=cf_dtraj.duration)
+        time.sleep(cf_dtraj.duration)
+
+    cf_dtraj=Design_trajectory("circle",20.0,1.0,cf)
+    n_wp=len(cf_dtraj.waypoints)
+    for i in range(0,n_wp):
+        cf.cmdPosition(pos=cf_dtraj.waypoints[i],yaw=cf_dtraj.yaw)
+        time.sleep(cf_dtraj.duration)
+
+    cf_dtraj=Design_trajectory("eight",30.0,1.0,cf)
+    n_wp=len(cf_dtraj.waypoints)
+    for i in range(0,n_wp):
+        cf.cmdPosition(pos=cf_dtraj.waypoints[i],yaw=cf_dtraj.yaw)
         time.sleep(cf_dtraj.duration)
 
     cf.land(targetHeight = 0.0, duration = 5.0)
